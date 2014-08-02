6b00022215c73b473253681f6144d49d39230cfc
Ed Scheinerman <ers@jhu.edu>
2014-08-02 19:08:30 -0400

    Added runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,26 @@
+using Base.Test
+using ClosedIntervals
+
+A = ClosedInterval(1,5)
+B = ClosedInterval(7,9)
+C = ClosedInterval(9,1)
+
+@test A+B == C
+@test isempty(A*B)
+@test A == ClosedInterval(right(A),left(A))
+@test length(A) == 4
+
+A = ClosedInterval(4.)
+B = ClosedInterval(1.)
+C = ClosedInterval(4.,1.)
+
+@test A+B == C
+@test length(A)==0
+@test isempty(A*B)
+
+A = ClosedInterval(1,6)
+B = ClosedInterval(8,3)
+C = ClosedInterval(3,6)
+
+@test A*B == C
+@test A+B == ClosedInterval(1,8)
